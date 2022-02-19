eb91a9d50fa068646a9f0f75d04ed09dcab79f96
quangbui205 <bmq205999@gmail.com>
2022-02-19 14:26:16 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$lang = "PHP";
+$return = print $lang." i love it."; //thay bằng echo sẽ xảy ra lỗi
+print "</br>";
+print "Value return: ".$return;
+?>
+
+</body>
+</html>
